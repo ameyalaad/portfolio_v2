ff688a828a0f3044bc66b27b8a5fad7de49b96e3
Ameya <ameyalaad200@gmail.com>
2020-07-19 14:46:15 +0530

    Updates

---
+++ precache-manifest.a6e7f5a4e111660367741089fd784702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b71b8ab16a4ff3e0111bfbea19bc0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a9d10a7eeb8878d4886d",
+    "url": "/react-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3aac042792b1606680bd",
+    "url": "/react-portfolio/static/js/2.33ae6a19.chunk.js"
+  },
+  {
+    "revision": "eff758343d82f754e4881ed2e608a50b",
+    "url": "/react-portfolio/static/js/2.33ae6a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d10a7eeb8878d4886d",
+    "url": "/react-portfolio/static/js/main.78402156.chunk.js"
+  },
+  {
+    "revision": "ea608e5f497480426bb5",
+    "url": "/react-portfolio/static/js/runtime-main.7540febe.js"
+  }
+]);
